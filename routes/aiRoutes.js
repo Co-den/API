10fd23ec2fe6bc7d17bc1c10c8f833c5d81248a3
co-den <iamagugbueikenna@gmail.com>
2025-07-14 18:26:05 +0100

    another commit

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { chatWithGemini } = require('../controllers/aiController');
+const chatWithGemini = require('../controllers/aiController');
 
 const router = express.Router();
 
-router.post('/gemini', chatWithGemini);
+router.post('/gemini', chatWithGemini.askGemini);
 
 module.exports = router;
