bd960628381b434f2af3821611dcbe125ba6fed9
co-den <iamagugbueikenna@gmail.com>
2025-07-14 19:06:19 +0100

    another commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ dotenv.config({ path: "./config.env" });
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "90d",
+    expiresIn: process.env.JWT_EXPIRES_IN || "90",
   });
 };
 
@@ -14,7 +14,7 @@ const createSendToken = (user, statusCode, req, res) => {
 
   const cookieOptions = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production'
