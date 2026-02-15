dfdbec181328a19484b1cae89718c37b420be736
co-den <iamagugbueikenna@gmail.com>
2026-02-15 09:48:54 +0100

    updated userschema

--- models/User.js
+++ models/User.js
@@ -5,135 +5,73 @@ const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema(
   {
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      trim: true,
-      validate: [validator.isEmail, "Please provide a valid email"],
-    },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: {
       type: String,
       required: [true, "Password is required"],
       minlength: 6,
-      select: false,
+      maxlength: 8,
     },
     passwordConfirm: {
       type: String,
-      required: [true, "Please confirm your password"],
+      required: [false],
       validate: {
-        
         validator: function (value) {
           return value === this.password;
         },
         message: "Passwords do not match",
       },
+      passwordChangedAt: { type: Date },
+      passwordResetToken: { type: String },
+      passwordResetExpires: { type: Date },
+      active: {
+        type: Boolean,
+        default: true,
+        select: false,
+      },
     },
-
-    passwordChangedAt: {
-      type: Date,
-    },
-    passwordResetToken: {
-      type: String,
-    },
-    passwordResetExpires: {
-      type: Date,
-    },
-    active: {
-      type: Boolean,
-      default: true,
-      select: false,
-    },
-    state: {
-      type: String,
-      required: true,
-    },
-    country: {
-      type: String,
-      required: true,
-    },
+    state: { type: String, required: true },
+    country: { type: String, required: true },
   },
-  {
-    timestamps: true,
-  },
+  { timestamps: true }
 );
 
-// Encrypt password before saving
 userSchema.pre("save", async function (next) {
-  // Only run if password was modified
   if (!this.isModified("password")) return next();
 
-  console.log("Hashing password for user:", this.email);
-
-  // Hash password with cost of 12
   this.password = await bcrypt.hash(this.password, 12);
-
-  // Delete passwordConfirm field (we don't want to store it)
   this.passwordConfirm = undefined;
-
   next();
 });
-
-// Update passwordChangedAt when password is modified (except on new documents)
-userSchema.pre("save", function (next) {
-  if (!this.isModified("password") || this.isNew) return next();
-
-  // Set passwordChangedAt to 1 second in the past to ensure token is created after password change
-  this.passwordChangedAt = Date.now() - 1000;
-  next();
-});
-
-// Instance method to check if password is correct
 userSchema.methods.correctPassword = async function (
   candidatePassword,
-  userPassword,
+  userPassword
 ) {
   return await bcrypt.compare(candidatePassword, userPassword);
 };
-
-// Check if password was changed after JWT token was issued
 userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
   if (this.passwordChangedAt) {
     const changedTimestamp = parseInt(
       this.passwordChangedAt.getTime() / 1000,
-      10,
+      10
     );
     return JWTTimestamp < changedTimestamp;
   }
-  // False means password was NOT changed
   return false;
 };
-
-// Generate password reset token
 userSchema.methods.createPasswordResetToken = function () {
   const resetToken = crypto.randomBytes(32).toString("hex");
-
   this.passwordResetToken = crypto
     .createHash("sha256")
     .update(resetToken)
     .digest("hex");
-
-  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; // 10 minutes
   return resetToken;
 };
 
-// Query middleware to exclude inactive users
-userSchema.pre(/^find/, function (next) {
-  // this points to current query
-  this.find({ active: { $ne: false } });
-  next();
-});
 
-const User = mongoose.model("User", userSchema);
+const userModel = mongoose.model("User", userSchema);
 
-module.exports = User;
+module.exports = userModel;
